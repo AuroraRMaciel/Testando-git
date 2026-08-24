4211b6c4074e28e2c0146de1c7c82ebfbf3d9354
Aurora Rodrigues Maciel <auroramaciel62@gmail.com>
2026-08-24 15:13:48 -0300

    Meu segundo commit

---
+++ calculadora.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main (){
+    float a, b, c;
+    int op = 0;
+
+    while (op < 5){
+       printf ("Digite a opcao desejada: \n1 - Adcao\n2 - Subtracao\n3 - Multiplicacao\n4 - Divisao");
+
+    }
+
+    return 0;
+}
